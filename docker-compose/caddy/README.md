5552672671f9347464c0922062db1eeb2029cab3
Mr.Red <berlitz@cyb3rcave.net>
2022-09-23 17:42:19 +0200

    🚀 Update Caddy to xCaddy

---
+++ docker-compose/caddy/README.md
@@ -0,0 +1,13 @@
+# xCaddy with Cloudfalre plugin
+
+## About Caddy
+Caddy simplifies your infrastructure. It takes care of TLS certificate renewals, OCSP stapling, static file serving, reverse proxying, Kubernetes ingress, and more.
+Its modular architecture means you can do more with a single, static binary that compiles for any platform.
+Caddy runs great in containers because it has no dependencies—not even libc. Run Caddy practically anywhere.
+
+## Setup / use
+1. Clone the repo
+2. Navigate into this directory
+3. Run the script `caddy-dependensies.sh`, only if when ceaping the default values in the `docker-compose.yml` file
+4. Start the container `docker compose up -d`
+ 
--- docker-compose/caddy/caddy-dependensies.sh
+++ docker-compose/caddy/caddy-dependensies.sh
@@ -1,9 +1,81 @@
 #!/bin/bash
+# ---
+# Use this script when continuing to use the default
+# vaules given in the docker-compose.yml file.
+# Do NOT use the script if you change the default 
+# values of the docker-compose.yml file!
+# ---
 
-# // Caddy dir in host os.
-# Create needed directories for caddy docker container.
-mkdir -p /etc/caddy
+# Root check
+# ---
+if [ $(id -u) -ne 0 ]
+then
+    echo "\n⚠️ \033[33mPlease run script as root!\033[m\n---\nThe spacestation script needs to be run with root user privileges.\n---"
+    exit
+fi
 
-# // Caddy files in host os.
-# Copy needed files for caddy docker container
-cp ./data/Caddyfile /etc/caddy/
+# General info of script
+clear
+echo "\033[36m
+# Use this script when continuing to use the default
+# vaules given in the docker-compose.yml file.
+# Do NOT use the script if you change the default 
+# values of the docker-compose.yml file!
+\033[m"
+while true
+do
+    input="N"
+    read -r -p "Do you want to run the script? [y/N] " input
+    case $input in
+        [yY])
+            break
+            ;;
+        [nN])
+            clear
+            exit
+            ;;
+        *)
+            echo "❔ Invalid input. Please try again!"
+            ;;
+    esac
+done
+
+
+# Create needed directories for Caddyfile
+# ---
+if [ ! -d "/etc/caddy" ]
+then
+    mkdir -p /etc/caddy
+    if [ ! -d "/etc/caddy" ]
+    then
+        echo "💢 Error creating '/etc/caddy' directory!"
+    else
+        echo "😃 Successful creation directory!"
+    fi
+else
+    echo "🤨 Directory '/etc/caddy' already exists!"
+fi
+
+# Copy Caddyfile
+# ---
+if [ ! -f /etc/caddy/Caddyfile ]
+then
+    cp ./data/Caddyfile /etc/caddy/
+else
+    echo "💢 Error '/etc/caddy/Caddyfile' already exists. Please move it before rerunning the script!"
+fi
+
+# Check for predefined docker network 'caddy_proxy'
+# ---
+if ! docker network ls | grep caddy_proxy > /dev/null
+then
+    docker network create -d bridge caddy_proxy
+    if ! docker network ls | grep caddy_proxy > /dev/null
+    then
+        echo "💢 Error creating docker network!"
+    else
+        echo "😃 Successful creation of docker network!"
+    fi
+else 
+    echo "🤨 Docker network already exists!"
+fi
